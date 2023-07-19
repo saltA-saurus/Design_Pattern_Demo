aa16f5d856767d31b316f2b7c22abe6b756e4f87
Jung Woo <33047537+saltA-saurus@users.noreply.github.com>
2023-07-18 22:30:56 -0400

    Delete code directory

--- code/Central_AI/Site.java
+++
@@ -1,7 +0,0 @@
-package Central_AI;
-
-public interface Site {
-    String receiveEncryptedMessage(String msg);
-    String receiveDecryptedMessage(String msg);
-    public void update();
-}
--- code/RPG/ArmourDecorator.java
+++
@@ -1,8 +0,0 @@
-package RPG;
-
-public abstract class ArmourDecorator implements Armour {
-    protected Armour armour;
-    public ArmourDecorator(Armour armour){
-        this.armour = armour;
-    }
-}
